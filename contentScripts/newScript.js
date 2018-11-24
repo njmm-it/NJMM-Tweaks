12d8191632fe4985f80fde5e899f6d039b72d3cb
ElderSansom <44982181+ElderSansom@users.noreply.github.com>
2018-11-23 21:01:42 -0500

    Update newScript.js

--- contentScripts/newScript.js
+++ contentScripts/newScript.js
@@ -526,7 +526,7 @@ function clickNextButton(buttonType, selector, scrollable = true) {
     var delay = generateDelayTime();    
 
     var nextButtonToPress = getNextButton(selector); /*We need to find the next button!*/
-    if (nextButtonToPress !== null && isVisble(nextButtonToPress)) { /*If the button exists, we should check if we should press it. If it doesn't exist, we scroll the page to see if we can generate more.*/
+    if (nextButtonToPress !== null) { /*If the button exists, we should check if we should press it. If it doesn't exist, we scroll the page to see if we can generate more.*/
         //if (canButtonsBeCurrentlyPressed === true && recentButtonsPressed < maximumFriendRequestsSent) {
         if (canButtonsBeCurrentlyPressed === true) {
             nextButtonToPress.scrollIntoView({
@@ -534,7 +534,11 @@ function clickNextButton(buttonType, selector, scrollable = true) {
                     block: "center",
                     inline: "nearest"
                 });
-            nextButtonToPress.click();
+			if (isVisble(nextButtonToPress)){
+				//If the button isn't visible, we shouldn't click it. This will avoid clicking already-clicked buttons.! Woo!
+				nextButtonToPress.click();
+			}
+
             nextButtonToPress.setAttribute("alreadyClicked", "true"); /*Our selectors should (theoretically) filter this object out on the next pass.*/
             /*checks the button type and deals with it as necessary.*/
             if (buttonType === "Add") {
