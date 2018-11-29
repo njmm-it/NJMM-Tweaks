f83fbe50eed87607f248042ca4931ed6a232ef01
ElderSansom <44982181+ElderSansom@users.noreply.github.com>
2018-11-28 20:51:36 -0500

    Update background.js

--- background/background.js
+++ background/background.js
@@ -1,15 +1,5 @@
 browser.runtime.onMessage.addListener(notify);
 
-
-while (true){
-    var getting = browser.tabs.getAllInWindow();
-    for (var tab in getting){
-        browser.pageAction.show(tab.windowID);
-    }
-}
-browser.pageAction.show(0)
-
-
 function notify(message,sender,sendResponse) {
     console.log('Incoming message from a content Script, sir!');
     if (message.pageLoaded){
@@ -18,7 +8,7 @@ function notify(message,sender,sendResponse) {
     if (message.addButtons === true || message.undoButtons === true){ //If the incoming message has AddButtons or undoButtons, then we will inject the script.
         var senderTabID = sender.id;  //A little redundant, but easier to read. This is the ID of the tab that sent the message, so that we inject the script into the correct tab.
         console.log(`The page on page ${senderTabID} has buttons!`);
-        browser.tabs.executeScript(senderTabID,{
+        browser.tabs.executeScript({
             file: "/contentScripts/newScript.js"
         });                                     //This injects the script.
     } else {    //Hopefully this never happens...
